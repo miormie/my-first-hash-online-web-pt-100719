6d86c50eaeb055e46c366cab6843bfd8688d94dc
Rose Seraphin <roseseraphin@yahoo.com>
2019-11-10 22:46:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
+shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
